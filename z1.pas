90b5551dacdb34277af3a261bb299121caaa286d
cipsiw <118716892+cipsiw@users.noreply.github.com>
2022-12-12 16:16:55 +0300

    Add files via upload

---
+++ z1.pas
@@ -0,0 +1,14 @@
+﻿begin
+  var  a: array[1..10] of integer;
+  var s: integer;
+  for var i := 1 to 10 do
+  begin
+    (a[i]) := readinteger;
+  end;
+  for var i := 1 to 10 do
+  begin
+    if (i mod 2 = 0) then
+      s += a[i];
+  end;
+  print('Сумма элементов, стоящих на четных позициях = ', s);
+end.
---
+++ z2.pas
@@ -0,0 +1,32 @@
+﻿begin
+  var s, ss: string;
+  var ls, lss, i: integer;
+  var f: boolean;
+  readln(s);
+  readln(ss);
+  i := 1;
+  l1 := length(s);
+  l2 := length(ss);
+  f := false;
+  while i <= ls - lss + 1 do
+  begin
+    if s[i] = ss[1] then
+    begin
+      f := true;
+      for var j := 1 to lss - 1 do
+        if s[i + j] <> ss[j + 1] then
+        begin
+          i := i + j;
+          f := false;
+          break
+        end;
+      if f = true then
+      begin
+        write('является');
+        exit;
+      end;
+    end;
+    i += 1;
+  end;
+  write('не является');
+end.
